14403efe2674931b200ca16e47f101a6ce2b4ec2
Kumar Kartikay <kumarkartikay005@gmail.com>
2024-08-27 01:28:45 +0530

    added resume

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,15 +5,20 @@ import 'tippy.js/dist/tippy.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+
 import './Home.css';
 
 export default function Center() {
     const handleGitHubClick = () => {
-        window.location.href = "https://github.com/KKartikay-27";
+        window.open("https://github.com/KKartikay-27", "_blank");
     };
     const handleLinkedInClick = () => {
-        window.location.href = "https://www.linkedin.com/in/kumarkartikay7224";
+        window.open("https://www.linkedin.com/in/kumarkartikay7224", "_blank");
     };
+    const handleResumeClick = () => {
+        window.open("https://drive.google.com/drive/folders/1oM5fxIHAunxH6TtixStDw-TmbsNmpYaC?usp=sharing","_blank");
+    }
     return (
         <div className="main">
             <div className="name">Kumar Kartikay</div>
@@ -27,6 +32,9 @@ export default function Center() {
                 </div>
                 <div className="qLinks linkedin" onClick={handleLinkedInClick}>
                     <FontAwesomeIcon icon={faLinkedin} size='2xl'/>
+                </div>
+                <div className="qLinks resume" onClick={handleResumeClick}>
+                    <FontAwesomeIcon icon={faFile} size='2xl'/>
                 </div>
                 <Tippy content="Email : kumarkartikay005@gmail.com">
                     <div className="qLinks mail">
